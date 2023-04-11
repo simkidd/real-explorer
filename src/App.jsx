0dc1e466c7be77f14a0a6054c27f11f1f8694e4c
John Mason <johnmsn22@gmail.com>
2023-04-11 18:17:21 +0100

    tablet view

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Layouts from './layouts/Layouts';
-import Advertise from './pages/Advertise';
-import Help from './pages/Help';
+// import Advertise from './pages/Advertise';
+// import Help from './pages/Help';
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
       <Route path='/' element={<Layouts />}>
         <Route index element={<Home/>} />
-        <Route path='advertise' element={<Advertise />} />
+        {/* <Route path='advertise' element={<Advertise />} /> */}
         {/* <Route path='help' element={<Help />} /> */}
       </Route>
       </Routes>
