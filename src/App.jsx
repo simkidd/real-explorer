c5f51184f8b34a22aec678b36562ea2364fdf195
John Mason <johnmsn22@gmail.com>
2023-04-11 18:26:00 +0100

    tablet view

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Layouts from './layouts/Layouts';
-// import Advertise from './pages/Advertise';
-// import Help from './pages/Help';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layouts from "./layouts/Layouts";
+import Advertise from "./pages/Advertise";
+import Help from "./pages/Help";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path='/' element={<Layouts />}>
-        <Route index element={<Home/>} />
-        {/* <Route path='advertise' element={<Advertise />} /> */}
-        {/* <Route path='help' element={<Help />} /> */}
-      </Route>
+        <Route path="/" element={<Layouts />}>
+          <Route index element={<Home />} />
+          <Route path="advertise" element={<Advertise />} />
+          <Route path="help" element={<Help />} />
+        </Route>
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
